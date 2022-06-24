2fbeebf34656acb9625f7121beb0c53a1ad71078
02awesome <81158572+02awesome@users.noreply.github.com>
2022-06-24 23:23:25 +0100

    repository that prints my name

---
+++ details.js
@@ -0,0 +1,18 @@
+let Name = "Ukachukwu Chukwuebuka";
+let height = '1.82m (6 ft 2")';
+let country = "Nigeria";
+// let details = Name + ", " + height + ", " + country;
+// console.log(details);
+
+document.write(
+  "My name is " +
+    Name +
+    ", " +
+    "I am " +
+    height +
+    " tall. " +
+    "I am a citizen of " +
+    country +
+    "."
+);
+// document.write(num1 + operator + num2 + " = " + result);
